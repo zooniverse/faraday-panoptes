c4dbacba7113fcdc8cd603e6bab732f340f61ac7
Amy Boyer <amy@zooniverse.org>
2017-03-21 12:48:33 -0500

    switch to rspec for testing

---
+++ spec/faraday/panoptes_spec.rb
@@ -0,0 +1,9 @@
+describe Faraday::Panoptes do
+  it 'has a version' do
+    expect(Faraday::Panoptes::VERSION).not_to be(nil)
+  end
+
+  it 'should be fine' do
+    expect(true).to be(true)
+  end
+end
